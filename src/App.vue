246869f6b677564f6b7e8defdfd8471ffb195240
bella <903165788@qq.com>
2019-07-31 00:33:51 +0800

    message

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap"> 
+       <Header :addTodo="addTodo" ></Header>
+        <List :todos="todos" :deletlTodo="deletlTodo"></List>
+        <Footer :todos="todos"  :selceAll="selceAll"  :deleteCompleted="deleteCompleted"></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import List from "./components/List.vue"
+  import Footer from "./components/Footer.vue"
+export default {
+  data (){
+    return {
+       todos:[
+          {id:1,title:"AAA",complete:true},
+          {id:2,title:"BBB",complete:false},
+          {id:3,title:"CCC",complete:true},
+      ]
+    }
+  },
+    components:{
+      Header,
+      List,
+      Footer
+    },
+
+    methods:{
+      addTodo(todo){
+        this.todos.unshift(todo)
+      },
+      deletlTodo(index){
+         this.todos.splice(index,1)
+      },
+      // 全选，全不选
+      selceAll(isCheck){
+         this.todos.forEach(todo=>todo.complete===isCheck)
+      },
+      // 删除已完成  l=留下的是false
+      deleteCompleted(){
+        this.todos = this.todos.filter(todo=>!todo.complete)
+      }
+    }
+}
+</script>
+
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
+
---
+++ src/components/Item.vue
@@ -0,0 +1,78 @@
+<template>  
+ <li :style="{background:bgColor}"
+  @mouseenter="hand(true)"  @mouseleave="hand(false)">
+    <label>
+    <input type="checkbox" v-model="todo.complete"/>
+    <span>{{todo.title}}</span>
+    </label>
+    <button class="btn btn-danger"  v-show="isShow" @click="deleteItem">删除</button>
+</li>
+</template>
+
+<script >    
+  export default {
+    data(){
+      return{
+        bgColor:"white",
+        isShow:false,
+        index:Number
+      }
+    },
+    props :{//属性名/属性值的类型
+      todo:Object,
+      deletlTodo:Function
+    },
+    methods:{
+      hand(isEnter){
+        if(isEnter){
+          this.bgColor = "#ccc",
+          this.isShow = true
+        }else{
+         this.bgColor = "white",
+          this.isShow = false
+      }
+    },
+    deleteItem(){
+      if( confirm("你确定删除吗？")){
+          this.deletlTodo(this.index)
+      }
+    }
+  }
+  }
+</script>
+
+<style scoped>
+ 
+    li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+    }
+
+    li label {
+    float: left;
+    cursor: pointer;
+    }
+
+    li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+    }
+
+    li button {
+    float: right;
+    margin-top: 3px;
+    }
+
+    li:before {
+    content: initial;
+    }
+
+    li:last-child {
+    border-bottom: none;
+    } 
+</style>
